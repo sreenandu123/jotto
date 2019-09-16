dbb4bc7615f42114888778847bda5769c429e53e
Sreenandanan <sreenandanan.sk@blackbuck.com>
2019-09-16 11:48:23 +0530

    guesssed words component started

---
+++ src/GuessedWord.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const GuessedWords = () => {
+    return (<div />)
+}
+
+GuessedWords.PropTypes = {
+
+}
+
+export default GuessedWords;
---
+++ src/GuessedWords.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow} from "enzyme";
+import checkPropTypes from "check-prop-types";
+import {findByTestAttr, checkProp} from "../test/testUtils";
+import GuessedWords from "./GuessedWord";
+
+test('does', () => {
+
+})
